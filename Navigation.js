b9bc1ec7c64c3de4033347c7ab8d058267b09905
Jose Fabian Beltran <joshefbeltran@gmail.com>
2023-09-06 10:01:56 -0500

    All dependeces were install

---
+++ Navigation.js
@@ -0,0 +1,8 @@
+import { NavigationContainer } from "@react-navigation/native";
+import Feed from "./screens/Feed";
+
+export default function Navigation() {
+  return <NavigationContainer>
+    <Feed/>
+  </NavigationContainer>
+}
