86b370377b9671e1d427b1f0e61a94ec4e025dc9
Jose Fabian Beltran <joshefbeltran@gmail.com>
2023-09-06 15:54:02 -0500

    TabNavigation it is working

--- Navigation.js
+++ Navigation.js
@@ -1,8 +1,41 @@
 import { NavigationContainer } from "@react-navigation/native";
-import Feed from "./screens/Feed";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import Feed from "./screens/tabScreens/Feed";
+import Notifications from "./screens/tabScreens/Notifications";
+import Settings from "./screens/tabScreens/Settings";
+import { Ionicons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+function TabGroup() {
+  return (
+    <Tab.Navigator 
+      screenOptions={({ route }) => ({
+        tabBarShowLabel: false,
+        tabBarIcon: ({ color, focused, size }) => {
+          let iconName;
+          if(route.name === "Feed") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Notifications"){
+            iconName = focused ? "ios-notifications" : "notifications-outline"
+          } else if ( route.name === "Settings") {
+            iconName = focused ? "settings" : "ios-settings-sharp"
+          }
+          return <Ionicons name={iconName} color={color} size={size} />
+        }
+      })}
+    >
+      <Tab.Screen name="Feed" component={Feed}/>
+      <Tab.Screen name="Notifications" component={Notifications}></Tab.Screen>
+      <Tab.Screen name="Settings" component={Settings}></Tab.Screen>
+    </Tab.Navigator>
+  );
+}
 
 export default function Navigation() {
-  return <NavigationContainer>
-    <Feed/>
-  </NavigationContainer>
+  return (
+    <NavigationContainer>
+      <TabGroup/>
+    </NavigationContainer>
+  )
 }
--- screens/tabScreens/Settings.jsx
+++ screens/tabScreens/Settings.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const Feed = () => {
+const Settings = () => {
   return (
     <View>
-      <Text>Feed</Text>
+      <Text>Settings</Text>
     </View>
   )
 }
 
-export default Feed
+export default Settings
